dfa1dc7b4d270390b6b41c1121ee308f5aeeca5c
FinnMcLaughlin <C15536837@mydit.ie>
2020-07-12 20:39:52 +0100

    Created new table in database to store request logs, which will be emptied every hour after the information is transferred into the hourly_stats table. Added function to log each request, checking whether the requestt is valid or not, and storing the data into the new requestLog table. Updated some function comments

--- src/HTTPServer.java
+++ src/HTTPServer.java
@@ -5,9 +5,13 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.Statement;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,6 +21,10 @@ import com.sun.net.httpserver.HttpServer;
 		
 public class HTTPServer {
 
+	
+	//String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+	
+	
 	/* Main that creates localhost HttpServer using port 8085,
 	 sets the handler function to handle requests sent to the server,
 	 and starts server */
@@ -33,111 +41,6 @@ public class HTTPServer {
 			System.out.println(e);
 		}	
 		
-	}
-	
-	/* Function to format the request body string into a parameter/value Hashmap */
-	private static Map<String, String> formatRequestBody(String reqBody){
-		Map<String, String> info = new HashMap<String, String>();
-		
-		//Gets the parameter count based on the number of '&' characters found
-		//in the request body string
-		int paramCount = reqBody.length() - reqBody.replace("&", "").length() + 1;
-		System.out.println("Paramter Count: " + paramCount);
-		
-		//Check to ensure there are no null values within or at the end of the
-		//request body string. If so, inserts "null" into the specified location
-		//as to enable easier distinction for later
-		reqBody = reqBody.replace("=&", "=null&");
-		
-		if(reqBody.charAt(reqBody.length()-1) == '=') {
-			reqBody = reqBody.concat("null");
-		}
-		
-		//Extracts the parameters and values and stores them in the Hashmap
-		for(int index=0; index < paramCount; index++) {					
-			String key = reqBody.split("&")[index].split("=")[0];
-			String value = reqBody.split("&")[index].split("=")[1];
-			info.put(key, value);
-			
-//			System.out.println("\nKey: " + key + "  |  Value: " + value);
-		}
-		
-		return info;
-	}
-	
-	/* Function to validate the request body data */
-	public static boolean validateRequestBody(Map<String, String> params) {
-		boolean invalidRequest = false;
-		
-		System.out.println("-----------------------------------------");
-		
-		//Malformed JSON
-		for(Map.Entry<String, String> parameter : params.entrySet()) {			
-			if(parameter.getValue().compareTo("null") == 0) {
-				invalidRequest = true;
-				break;
-			}
-		}
-		
-		//Not in Customer Table || Disabled Customer
-		if(!invalidRequest) {
-			String customerCheck = getQueryResult("Customer", "id=" + params.get("customerID"), "active");
-			if(customerCheck.compareTo("") == 0 || customerCheck.compareTo("0") == 0) {
-				invalidRequest = true;
-			}
-		}
-		
-		//Blacklisted IP
-		if(!invalidRequest) {
-			String ipCheck = getQueryResult("ip_blacklist", "ip=" + params.get("remoteIP").replace(".", ""), "ip");
-			if(ipCheck.compareTo("") != 0) {
-				invalidRequest = true;
-			}
-		}
-		
-		//Blacklist User Agent
-		if(!invalidRequest) {
-			String userAgentCheck = getQueryResult("ua_blacklist", "ua= \'" + params.get("userID") + "\'", "ua");
-			if(userAgentCheck.compareTo("") != 0) {
-				invalidRequest = true;
-			}
-		}
-		
-		
-		return invalidRequest;
-	}
-	
-	/* Function to connect to the PSQL database and execute queries */
-	public static String getQueryResult(String table, String cond, String column) {
-		System.out.println("Establishing Connection to Database...");	
-		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "password"))
-		{
-			
-			System.out.println("Connection Established");
-			//System.out.println(cond);
-			String reqQuery = "SELECT * FROM " + table + " WHERE " + cond + ";";
-			
-			Statement stmt = connection.createStatement();
-	        ResultSet rs = stmt.executeQuery(reqQuery);
-	        String response = "";
-	
-	        //System.out.println("Iterating through Query Result");
-	        while ( rs.next() )
-	        {
-	        	response = response + rs.getString(column);
-	        }	
-	        
-	        //System.out.println("->" + response);
-	        
-	        return response;
-	
-		}
-		catch (Exception e) {
-	        System.out.println("Connection failure.");
-	        e.printStackTrace();
-	        
-	        return "Connection Failure";
-	    }
 	}
 	
 	/* Function to handle the requests sent to the server */
@@ -175,10 +78,201 @@ public class HTTPServer {
 			System.out.println("RemoteIP: " + params.get("remoteIP"));
 			System.out.println("Timestamp: " + params.get("timestamp"));
 			
-			System.out.println(validateRequestBody(params));
+			boolean validRequest = validateRequestBody(params);
+			System.out.println(validRequest);
+			logRequest(params, validRequest);
 		}
 		else {
 			System.out.println("Not POST Request");
 		}
 	 }
+	
+	
+	
+	
+	/* Function to format the request body string into a parameter/value Hashmap */
+	public static Map<String, String> formatRequestBody(String reqBody){
+		// Formats the given request body string to be further utilised by
+		// getting the parameter count, based on the number of & characters in
+		// the request body string, ensures that any missing data is explicitly
+		// stated, and then extracts the parameters and their values and puts them
+		// into a hashmap		
+		
+		Map<String, String> info = new HashMap<String, String>();
+		
+		int paramCount = reqBody.length() - reqBody.replace("&", "").length() + 1;
+		System.out.println("Paramter Count: " + paramCount);
+		
+		reqBody = reqBody.replace("=&", "=null&");
+		
+		if(reqBody.charAt(reqBody.length()-1) == '=') {
+			reqBody = reqBody.concat("null");
+		}
+		
+		for(int index=0; index < paramCount; index++) {					
+			String key = reqBody.split("&")[index].split("=")[0];
+			String value = reqBody.split("&")[index].split("=")[1];
+			info.put(key, value);
+			
+//			System.out.println("\nKey: " + key + "  |  Value: " + value);
+		}
+		
+		return info;
+	}
+	
+	/* Function to validate the request body data */
+	public static boolean validateRequestBody(Map<String, String> params) {
+		// Checks whether the data passed through the request body is valid,
+		// based on whether it is or isn't malformed, whether the customer 
+		// exists in the database, whether the customer holds an active
+		// account, and whether or not the IP address and the User Agent
+		// is blacklisted		
+		
+		boolean validRequest = true;
+		
+		System.out.println("-----------------------------------------");
+		
+		//Malformed JSON
+		for(Map.Entry<String, String> parameter : params.entrySet()) {			
+			if(parameter.getValue().compareTo("null") == 0) {
+				validRequest = false;
+				break;
+			}
+		}
+		
+		//Not in Customer Table || Disabled Customer
+		if(validRequest) {
+			String customerCheck = getQueryResult("Customer", "id=" + params.get("customerID"), "active");
+			if(customerCheck.compareTo("") == 0 || customerCheck.compareTo("0") == 0) {
+				validRequest = false;
+			}
+		}
+		
+		//Blacklisted IP
+		if(validRequest) {
+			String ipCheck = getQueryResult("ip_blacklist", "ip=" + params.get("remoteIP").replace(".", ""), "ip");
+			if(ipCheck.compareTo("") != 0) {
+				validRequest = false;
+			}
+		}
+		
+		//Blacklist User Agent
+		if(validRequest) {
+			String userAgentCheck = getQueryResult("ua_blacklist", "ua= \'" + params.get("userID") + "\'", "ua");
+			if(userAgentCheck.compareTo("") != 0) {
+				validRequest = false;
+			}
+		}
+		
+		
+		return validRequest;
+	}
+	
+	/* Function to connect to the PSQL database and execute queries */
+	public static String getQueryResult(String table, String cond, String column) {
+		// Executes a SELECT query to the postgresql database based on the table,
+		// condition(s) passed into the function, and returns the data returned
+		// from the specified column also passed into the function
+		
+		System.out.println("Establishing Connection to Database...");	
+		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "password"))
+		{
+			
+			System.out.println("Connection Established");
+			//System.out.println(cond);
+			String reqQuery = "SELECT * FROM " + table + " WHERE " + cond + ";";
+			
+			Statement stmt = connection.createStatement();
+	        ResultSet rs = stmt.executeQuery(reqQuery);
+	        String response = "";
+	
+	        //System.out.println("Iterating through Query Result");
+	        while ( rs.next() )
+	        {
+	        	response = response + rs.getString(column);
+	        }	
+	        
+	        //System.out.println("->" + response);
+	        
+	        return response;
+	
+		}
+		catch (Exception e) {
+	        System.out.println("Connection failure.");
+	        e.printStackTrace();
+	        
+	        return "Connection Failure";
+	    }
+	}
+
+	/* Function log each request made to the server, based on the customerID and whether the
+	   request made was a valid request or not */
+	public static void logRequest(Map<String, String> params, boolean validRequest) {
+		// Check to see if the customerID exists in the requestLog table
+		// If customerID does not exist in the table, then the logQuery is written as an INSERT, and the data is initialised
+		// If customerID does exist in the table, then the logQuery is written as an UPDATE, incrementing the specified column data
+		// The validRequest boolean which is passed to the function is used to know which column's data is to be incremented when 
+		// logging the request
+		
+		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "password"))
+		{
+			
+			String logQuery = "";
+			String customerCheck = getQueryResult("requestLog", "customerId=" + params.get("customerID"), "customerId");			
+			
+			if(customerCheck.compareTo("") == 0) {				
+				String custID = params.get("customerID");
+								
+				int valid_request = 0;
+				int invalid_request = 0;
+				
+				if(validRequest) {
+					valid_request = valid_request + 1;
+				}
+				else {
+					invalid_request = invalid_request + 1;
+				}
+				
+				logQuery = "INSERT INTO requestLog VALUES (" + custID + "," + valid_request + "," + invalid_request + ");";
+			
+			}
+			else {				
+				String custID = params.get("customerID");
+				String update_column = "";
+				int update_count = -1;
+				
+				if(validRequest) {
+					update_column = "request_count";
+					update_count = Integer.parseInt(getQueryResult("requestLog", "customerId=" + params.get("customerID"), "request_count"));
+					System.out.println(update_count);
+				}
+				else {
+					update_column = "invalid_count";
+					update_count = Integer.parseInt(getQueryResult("requestLog", "customerId=" + params.get("customerID"), "invalid_count"));
+					System.out.println(update_count);
+				}
+				
+				if(update_column.compareTo("") != 0 && update_count >= 0) {
+					logQuery = "UPDATE requestLog SET " + update_column + "=" + (update_count + 1) + " WHERE customerID = " + custID + ";";
+				}
+				
+			}
+			
+			if(logQuery.compareTo("") != 0) {
+				PreparedStatement stmt = connection.prepareStatement(logQuery);
+		        int rs = stmt.executeUpdate();
+		        if(rs > 0) {
+		        	System.out.println("Request Log Successful");
+		        }
+		        else {
+		        	System.out.println("Request Log UnSuccessful");
+		        }
+			}
+		}
+		catch (Exception e) 
+		{
+	        System.out.println(e);
+	        e.printStackTrace();
+	    }
+	}
 }
